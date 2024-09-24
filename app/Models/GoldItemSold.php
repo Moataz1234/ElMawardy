2b4261ff312e7c60c69884f9f4c784bef9e83917
moataz <moataza630@gmail.com>
2024-09-24 10:16:12 +0300

    transfer items by buttons

---
+++ app/Models/GoldItemSold.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GoldItemSold extends Model
+{
+    use HasFactory;
+
+    // Specify the table name if it doesn't follow Laravel's naming convention
+    protected $table = 'gold_items_sold';
+
+    // Define the fillable fields
+    protected $fillable = [
+        'link',
+        'serial_number',
+        'shop_name',
+        'shop_id',
+        'kind',
+        'model',
+        'talab',
+        'gold_color',
+        'stones',
+        'metal_type',
+        'metal_purity',
+        'quantity',
+        'weight',
+        'add_date',
+        'source',
+        'to_print',
+        'price',
+        'semi_or_no',
+        'average_of_stones',
+        'net_weight',
+        'sold_date',
+    ];
+
+    // If you have any relationships, you can define them here
+}
