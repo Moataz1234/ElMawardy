043a2790c273d6702d72f0a2758b068b7a80e02a
moataz <moataza630@gmail.com>
2024-10-12 15:18:29 +0300

    style: Remove required attributes and comments from edit_image form fields

--- resources/views/shopify/edit_image.blade.php
+++ resources/views/shopify/edit_image.blade.php
@@ -10,23 +10,17 @@
         <input type="hidden" name="product_id" value="{{ $productId }}">
         <input type="hidden" name="image_id" value="{{ $imageId }}">
         
-        <label for="new_image_url">New Image URL:</label>
-        <input type="text" id="new_image_url" name="new_image_url" required>
+        {{-- <label for="new_image_url">New Image URL:</label>
+        <input type="text" id="new_image_url" name="new_image_url" required> --}}
 
         <label for="title">Title:</label>
-        <input type="text" id="title" name="title" required>
+        <input type="text" id="title" name="title" >
 
         <label for="description">Description:</label>
-        <textarea id="description" name="description" required></textarea>
-
-        <label for="vendor">Vendor:</label>
-        <input type="text" id="vendor" name="vendor" required>
+        <textarea id="description" name="description" ></textarea>
 
         <label for="product_type">Product Type:</label>
-        <input type="text" id="product_type" name="product_type" required>
-
-        <label for="tags">Tags (comma separated):</label>
-        <input type="text" id="tags" name="tags" required>
+        <input type="text" id="product_type" name="product_type" >
 
         <button type="submit">Update Product</button>
     </form>
