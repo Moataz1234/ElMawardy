1c02172e613a0fc9603d0c0e280f7c17044c85ba
moataz <moataza630@gmail.com>
2025-01-04 02:03:46 +0200

    new_talabat

--- database/migrations/2025_01_03_235424_create_talabat_table.php
+++ database/migrations/2025_01_03_235424_create_talabat_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('talabat', function (Blueprint $table) {
             $table->id();
+            $table->string('model')->unique();
+            $table->string('scanned_image')->nullable();
+            $table->string('stars')->nullable();
+            $table->string('source')->nullable();
+            $table->string('first_production')->nullable();
+            $table->string('semi_or_no')->nullable();
+            $table->decimal('average_of_stones', 10, 2)->nullable();
+
             $table->timestamps();
+        
         });
     }
 
